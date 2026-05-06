7a11be8c2aec4534c295e2ad8396e6ec1e63bc98
Sam Gallagher <samhgallagher@gmail.com>
2026-05-06 04:05:53 +0000

    issue with builds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,10 @@ export default defineNuxtConfig({
     enabled: true
   },
 
+  nitro: {
+    preset: 'aws-amplify'
+  },
+
   css: ['~/assets/css/main.css'],
 
   compatibilityDate: '2025-01-15',
